afaef1ccb949c3e39f4e6180dc6d9e9ab0523802
Lenovo <somusp75@gmail.com>
2022-01-19 22:37:41 +0530

    Add project files.

---
+++ JsonFile/FetchJsonForRice.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace JsonFile
+{
+   public  class FetchJsonForRice
+    {
+        public Rice ReadTheJsonFile(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ JsonFile/Rice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonFile
+{
+    public class Rice
+    {
+        public List<TypeofRice> typeOfRice;
+        public List<TypeofPulses> typeofPulses;
+        public List<TypeofWheats> typeofWheats;
+
+
+
+    }
+    public class TypeofRice
+    {
+        public string name;
+        public string weight;
+        public int price;
+       
+
+    }
+    public class TypeofPulses
+    {
+        public string name;
+        public string weight;
+        public int price;
+    }
+   public class TypeofWheats
+        {
+            public string name;
+            public string weight;
+            public int price;
+
+        }
+    }
+
+
+
+
